72d45daa1af23276ea18846cdeb3d8839a3ebc62
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-18 16:15:38 -0300

    Initial Commit

---
+++ alura-travel-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alura-travel-app
+//
+//  Created by Elisa Camillo on 18/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
