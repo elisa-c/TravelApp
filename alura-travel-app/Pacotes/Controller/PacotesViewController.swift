a0600b5756411a61f2e45d3fdbf8c42dc599fd4c
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-19 16:45:32 -0300

    :lipstick: spacing and colors

--- alura-travel-app/Pacotes/Controller/PacotesViewController.swift
+++ alura-travel-app/Pacotes/Controller/PacotesViewController.swift
@@ -30,9 +30,18 @@ class PacotesViewController: UIViewController, UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteCollectionViewCell
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PacoteCollectionViewCell
         
-        celulaPacote.backgroundColor = UIColor.blue
+        let viagemAtual = listaViagens[indexPath.item]
+        
+        celulaPacote.labelTitulo.text = viagemAtual.titulo
+        celulaPacote.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = "R$ \(viagemAtual.preco)"
+        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 85.0/255.0).cgColor
+        celulaPacote.layer.cornerRadius = 8
         
         return celulaPacote
     }
