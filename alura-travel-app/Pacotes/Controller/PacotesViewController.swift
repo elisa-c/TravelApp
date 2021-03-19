648903fdc548df6cddb495dae82f6b6d323a808e
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-19 10:36:20 -0300

    :lipstick: constraints updated

---
+++ alura-travel-app/Pacotes/Controller/PacotesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PacotesViewController.swift
+//  alura-travel-app
+//
+//  Created by Elisa Camillo on 19/03/21.
+//
+
+import UIKit
+
+class PacotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
