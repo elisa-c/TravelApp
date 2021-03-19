aab87a072cfa3e69ef65ba70869bb835eb24e4e9
Elisa Camillo <elisatcamillo@gmail.com>
2021-03-19 13:38:42 -0300

    :lipstick: updating constraints to fit iphone SE

--- alura-travel-app/Pacotes/Controller/PacotesViewController.swift
+++ alura-travel-app/Pacotes/Controller/PacotesViewController.swift
@@ -7,23 +7,37 @@
 
 import UIKit
 
-class PacotesViewController: UIViewController {
-
+class PacotesViewController: UIViewController, UICollectionViewDataSource {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        colecaoPacotesViagem.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
     
+    //
 
-    /*
+    let listaViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    //
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.listaViagens.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteCollectionViewCell
+        
+        celulaPacote.backgroundColor = UIColor.blue
+        
+        return celulaPacote
+    }
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ alura-travel-app/Pacotes/View/PacoteCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PacoteCollectionViewCell.swift
+//  alura-travel-app
+//
+//  Created by Elisa Camillo on 19/03/21.
+//
+
+import UIKit
+
+class PacoteCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imagemViagem: UIImageView!
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+
+    
+}
